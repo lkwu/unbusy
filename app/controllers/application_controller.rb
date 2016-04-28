c44f0bdf2ec7f2de3124a8da7a3cb0490a37b17c
Bruce <lywctl@gmail.com>
2016-04-28 21:20:09 +0800

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,5 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || new_booking_path
   end
+  
 end
