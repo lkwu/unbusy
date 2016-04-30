b93ecd6e807223090f364cbfbdc24786c8110631
lkwu <legray1109@gmail.com>
2016-04-30 16:33:22 +0800

    Discard profile model, controller and views

--- db/migrate/20160429140214_create_profiles.rb
+++
@@ -1,8 +0,0 @@
-class CreateProfiles < ActiveRecord::Migration
-  def change
-    create_table :profiles do |t|
-      t.integer :user_id, :index => true
-      t.timestamps null: false
-    end
-  end
-end
