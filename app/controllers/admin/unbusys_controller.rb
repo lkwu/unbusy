9fe680a32eebf7395c21e2cf0c03a9985ad7895f
lkwu <legray1109@gmail.com>
2016-05-16 00:56:55 +0800

    Modified Admin authentication

--- app/controllers/admin/unbusys_controller.rb
+++ app/controllers/admin/unbusys_controller.rb
@@ -1,18 +1,11 @@
 class Admin::UnbusysController < ApplicationController
 
-  before_action :authenticate
+  before_action :authenticate_user!
+  before_action :check_admin
   layout "admin"
 
   def index
     @bookings = Booking.all
-  end
-
-  protected
-
-  def authenticate
-     authenticate_or_request_with_http_basic do |user_name, password|
-         user_name == "unbusyadmin" && password == "unbusyadmin"
-     end
   end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,20 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
   	if params[:look_bookings]
-  	  user_path(current_user) 
+  	  user_path(current_user)
     elsif params[:booking_id]
       edit_user_path(current_user, :booking_id => params[:booking_id] )
-  	else	
+  	else
       request.env['omniauth.origin'] || stored_location_for(resource) || new_booking_path
     end
   end
-  
+
+  def check_admin
+    unless current_user.admin
+      flash[:alert] = "您無權限造訪此頁面"
+      redirect_to root_path
+      return
+    end
+  end
+
 end
